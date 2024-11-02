c96cf4dc516680e12eaa19d21d8d41db48329792
Bibek <137331749+immbibek@users.noreply.github.com>
2024-11-02 22:39:50 +0530

    authentication using clerk sign in sign out Done

---
+++ constants/index.ts
@@ -0,0 +1,29 @@
+export const sidebarLinks =[
+    {
+        label:"Home",
+        imgUrl:'./icons/Home.svg',
+        route:"/"
+    },
+    {
+        label:"Upcoming",
+        imgUrl:'./icons/upcoming.svg',
+        route:"/upcoming"
+    },
+    {
+        label:"Previous",
+        imgUrl:'./icons/previous.svg',
+        route:"/previous"
+    },
+    {
+        label:"Recording",
+        imgUrl:'./icons/Video.svg',
+        route:"/recordings"
+    },
+    {
+        label:"Personal Room",
+        imgUrl:'./icons/add-personal.svg',
+        route:"/personal-room"
+    },
+
+ 
+]
