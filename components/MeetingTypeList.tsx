398e947a4effc5b4fa673c501e08e61c4a03ba51
Bibek <137331749+immbibek@users.noreply.github.com>
2024-11-04 00:15:23 +0530

    working on stream on Meeting typelist

--- components/MeetingTypeList.tsx
+++ components/MeetingTypeList.tsx
@@ -3,10 +3,57 @@
 import { useState } from 'react'
 import HomeCard from './HomeCard'
 import { useRouter } from 'next/navigation'
+import MeetingModel from './MeetingModel'
+import { useUser } from '@clerk/nextjs'
+import { Call, useStreamVideoClient } from '@stream-io/video-react-sdk'
+
 const MeetingTypeList = () => {
     const router=useRouter();
-
+   
     const [meetingState,setMeetingState]=useState<'isScheduleMeeting' | 'isjoiningMeeting' | 'isInstantMeeting'| undefined>()
+    const {user} =useUser();
+    const client=useStreamVideoClient();
+
+    const [values,setValues]=useState({
+      dateTime: new Date(),
+      description:'',
+      link:''
+    })
+     
+    const [callDetails,setCallDetails]=useState<Call>()
+
+
+    const createMeeting= async ()=>{
+      if(!client || !user) return;
+
+      try {
+        const id=crypto.randomUUID();
+        const call=client.call('default',id);
+
+        if(!call) throw new Error("Failed to create call")
+
+          const startsAt =values.dateTime.toISOString() || new Date(Date.now()).toISOString();
+
+          const description=values.description || 'Instantmeeting';
+
+          await call.getOrCreate({
+            data:{
+              starts_at:startsAt,
+              custom:{
+                description
+              }
+            }
+          })
+          setCallDetails(call)
+
+          if(!values.description){
+            router.push(`/meeting/${call.id}`)
+          }
+      } catch (error) {
+        console.log(error);
+      }
+    }
+
   return (
     <section className='grid grid-cols-1 gap-5 md:grid-cols-2 xl:grid-cols-4'>
           {/* BOX 1 */}
@@ -14,7 +61,7 @@ const MeetingTypeList = () => {
           img="/icons/add-meeting.svg"
           title="New Meeting"
           description="Start an instant meeting"
-          handleClick={()=>setMeetingState('isjoiningMeeting')}
+          handleClick={()=>setMeetingState('isInstantMeeting')}
           className="bg-orange-1"
         />
         <HomeCard
@@ -40,6 +87,15 @@ const MeetingTypeList = () => {
         />
   
         
+        <MeetingModel
+          isOpen={meetingState=== 'isInstantMeeting'}
+          
+          onClose={()=>setMeetingState(undefined)}
+          title="Start an instant Meeting"
+          className="text-center"
+          buttonText="Start Meeting"
+          handleClick={createMeeting}
+        />
 
     </section>
   )
